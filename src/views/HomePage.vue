7693ab5496f3c16336deebf72be38bd92e1e4636
Luc Boulanger <luc.boulanger98@gmail.com>
2023-06-22 00:43:10 +0200

    Update Profil

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,18 +1,26 @@
 <script>
 import { accountService } from "../_services/account.service";
 import { userService } from "../_services/user.service";
+import ModalProfil from "../components/ModalProfil.vue";
 export default {
   name: "HomePage",
+  components: {
+    modale: ModalProfil,
+  },
   data() {
     return {
       token: null,
       userData: [],
+      modalOpen: false,
     };
   },
   mounted() {
     this.GetUser();
   },
   methods: {
+    toogleModale: function () {
+      this.modalOpen = !this.modalOpen;
+    },
     GetUser() {
       userService
         .getUser()
@@ -31,30 +39,75 @@ export default {
 <template>
   <header>
     <img src="../../public/img/logo.svg.svg" class="logo" />
-    <div class="profil">
+    <div class="profil" @click="toogleModale">
       <p class="usernameText">
         {{ userData.username ? userData.username.charAt(0) : "" }}
       </p>
     </div>
   </header>
+  <modale
+    :userData="userData"
+    :modaleOpen="modalOpen"
+    :toogleModale="toogleModale"
+  />
 </template>
+
 <style>
-header {
-  display: flex;
-}
-.logo {
-  width: 5vh;
-}
-.profil {
-  background-color: red;
-  width: 5vh;
-  height: 5vh;
-  display: flex;
-  justify-content: center;
-  border-radius: 50%;
+@media (max-width: 767px) {
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-left: 7%;
+    margin-right: 7%;
+    margin-top: 5%;
+  }
+
+  .logo {
+    width: 6vh;
+  }
+
+  .profil {
+    background-color: red;
+    width: 5vh;
+    height: 5vh;
+    display: flex;
+    justify-content: center;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  .usernameText {
+    font-size: 100%;
+  }
 }
 
-.usernameText {
-  font-size: 100%;
+@media (min-width: 768px) {
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-left: 10%;
+    margin-right: 10%;
+    margin-top: 5%;
+  }
+
+  .logo {
+    width: 8vh;
+  }
+
+  .profil {
+    background-color: red;
+    width: 6vh;
+    height: 6vh;
+    display: flex;
+    justify-content: center;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  .usernameText {
+    font-size: 120%;
+  }
 }
 </style>
